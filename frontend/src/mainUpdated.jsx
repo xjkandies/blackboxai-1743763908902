e016b95f2624d6835d1cf7ba51d20e6aadec5708
xjkandies <xjkandies@gmail.com>
2025-04-04 11:57:15 +0100

    Add frontend/src/mainUpdated.jsx 1743764236591

---
+++ frontend/src/mainUpdated.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import AppUpdated from './AppUpdated';
+import './styles.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AppUpdated />
+  </React.StrictMode>
+);
